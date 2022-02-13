785af55323047517b46bc8226be3edd5cdc2fec0
yuukiuvw <98954743+yuukiuvw@users.noreply.github.com>
2022-02-13 11:24:15 +0900

    プルダウンの実装

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2022_02_12_052155) do
   end
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
