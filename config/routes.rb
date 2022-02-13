ad12cfd8adb084b1135e9bbcb7e6313831e28671
yuukiuvw <98954743+yuukiuvw@users.noreply.github.com>
2022-02-13 17:56:34 +0900

    空のメッセージが送信できないバリデーションの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
-  esources :rooms, only: [:new, :create] do
+  resources :rooms, only: [:new, :create] do
     resources :messages, only: [:index, :create]
   end
 end
